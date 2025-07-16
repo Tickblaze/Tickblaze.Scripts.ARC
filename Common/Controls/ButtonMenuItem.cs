e327937339b5ab61e947009aa4a6aa134d3ba839
Nikita Zalessky <nikita@neurostreet.com>
2025-07-16 23:00:59 +0300

    Added support for commands in ButtonMenuItem. Styled ButtonMenuItem. Centered OnOffMenuItem text.

--- Common/Controls/ButtonMenuItem.cs
+++ Common/Controls/ButtonMenuItem.cs
@@ -1,5 +1,23 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Tickblaze.Community;
 
-public class ButtonMenuItem : MenuItem;
+public class ButtonMenuItem : MenuItem
+{
+    public static readonly DependencyProperty ActionCommandProperty = DependencyProperty.Register(nameof(ActionCommand), typeof(ICommand), typeof(ButtonMenuItem));
+    public static readonly DependencyProperty ActionCommandParameterProperty = DependencyProperty.Register(nameof(ActionCommandParameter), typeof(object), typeof(ButtonMenuItem));
+    
+    public ICommand ActionCommand
+    {
+        get => (ICommand) GetValue(ActionCommandProperty);
+        set => SetValue(ActionCommandProperty, value);
+    }
+
+    public object? ActionCommandParameter
+    {
+        get => GetValue(ActionCommandParameterProperty);
+        set => SetValue(ActionCommandParameterProperty, value);
+    }
+}
--- Common/Controls/OnOffMenuItem.cs
+++ Common/Controls/OnOffMenuItem.cs
@@ -5,11 +5,6 @@ namespace Tickblaze.Community;
 
 public class OnOffMenuItem : MenuItem
 {
-    static OnOffMenuItem()
-    {
-		DefaultStyleKeyProperty.OverrideMetadata(typeof(OnOffMenuItem), new FrameworkPropertyMetadata(typeof(OnOffMenuItem)));
-    }
-
     public static readonly DependencyProperty IsOnProperty = DependencyProperty.Register(nameof(IsOn), typeof(bool), typeof(OnOffMenuItem));
     public static readonly DependencyProperty IsThreeStateProperty = DependencyProperty.Register(nameof(IsThreeState), typeof(bool), typeof(OnOffMenuItem), new(false));
     
